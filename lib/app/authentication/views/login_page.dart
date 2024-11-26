3f196607e2b38d86c007334e6278690cd230695b
Yukesh <katuwalukesh@gmail.com>
2024-11-26 09:31:35 +0545

    animated login page made

--- lib/app/authentication/views/login_page.dart
+++ lib/app/authentication/views/login_page.dart
@@ -1,9 +1,26 @@
+import 'package:chatapp/app/home/views/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isAnimate = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(milliseconds: 500));
+    setState(() {
+      _isAnimate = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,53 +28,85 @@ class LoginPage extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding:
-              EdgeInsets.only(left: 16.w, right: 16.w, top: 16.h, bottom: 45.h),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                child: Text(
-                  "Welcome to We chat",
-                  style: TextStyle(color: Colors.black, fontSize: 24.sp),
-                ),
-              ),
-              SizedBox(
-                height: height * 0.1,
-              ),
-              Image.asset(
-                "assets/images/chat.png",
-                height: 350.h,
-                width: 350.w,
-              ),
-              Spacer(),
-              SizedBox(
-                height: 55.h,
-                width: double.infinity,
-                child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey.shade400,
-                    shape: StadiumBorder(),
-                    elevation: 1,
-                  ),
-                  icon: SvgPicture.asset(
-                    "assets/icons/google_logo.svg",
-                    height: 20.h,
-                    width: 20.w,
-                  ),
-                  onPressed: () {
-                    // Add your sign-in logic here
-                  },
-                  label: Text(
-                    "Sign In with Google",
-                    style: TextStyle(fontSize: 16.sp, color: Colors.black),
-                  ),
-                ),
-              ),
-            ],
+        //appbar
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Welcome to We Chat",
+            style: TextStyle(color: Colors.black, fontSize: 24.sp),
           ),
+        ),
+        body: Stack(
+          children: [
+            // Centered Image
+            AnimatedPositioned(
+              duration: Duration(microseconds: 1200),
+              child: Positioned(
+                right: !_isAnimate ? 1.h : width * 0.5,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 90.h,
+                    ),
+                    Image.asset(
+                      "assets/images/chat.png",
+                      height: 300.h,
+                      width: 300.w,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // Button at the bottom
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 90.h),
+                child: SizedBox(
+                  height: 55.h,
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey.shade400,
+                      shape: const StadiumBorder(),
+                      elevation: 1,
+                    ),
+                    icon: SvgPicture.asset(
+                      "assets/icons/google_logo.svg",
+                      height: 20.h,
+                      width: 20.w,
+                    ),
+                    onPressed: () {
+                      Get.offAll(() => const HomeScreen());
+                    },
+                    label: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Log in with ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black,
+                              fontSize: 16.sp,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Google",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                              fontSize: 16.sp,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
