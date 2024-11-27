ec979c1c0d9111e48387bb32a17368a8da7e51b1
Yukesh <katuwalukesh@gmail.com>
2024-11-27 19:51:54 +0545

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:chatapp/app/authentication/views/login_page.dart';
 import 'package:chatapp/app/home/views/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDGcgSCk70VtXdu4ml5qcKhbU6hTqSNhrA",
+          appId: "1:1039950335044:android:5497d292a310e13d534011",
+          messagingSenderId: "1039950335044",
+          projectId: "wechat-a3953"));
+
   runApp(const MyApp());
 }
 
