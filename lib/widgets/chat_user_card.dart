86815b98ed78724f73309bd58f9f8787f3d8b7ef
Yukesh <katuwalukesh@gmail.com>
2024-12-05 06:20:12 +0545

    data from details loaded

--- lib/widgets/chat_user_card.dart
+++ lib/widgets/chat_user_card.dart
@@ -1,42 +1,56 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chatapp/app/authentication/model/auth_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class ChatUserCard extends StatelessWidget {
+class ChatUserCard extends StatefulWidget {
   const ChatUserCard({super.key, required this.user});
   final UserModel user;
 
   @override
+  State<ChatUserCard> createState() => _ChatUserCardState();
+}
+
+class _ChatUserCardState extends State<ChatUserCard> {
+  @override
   Widget build(BuildContext context) {
+    print("User image URL: ${widget.user.image}");
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
       child: InkWell(
         borderRadius: BorderRadius.circular(8.r),
         onTap: () {},
         child: ListTile(
-          //title
-          title: Text(
-            user.name!,
-            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16.sp),
-          ),
-          //subtitle
-          subtitle: Text(
-            user.about!,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontSize: 14.sp),
-          ),
-          //leading icon
-          leading: Icon(
-            CupertinoIcons.person,
-            size: 24.sp,
-          ),
-          trailing: Text(
-            "12:00 PM",
-            style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14.sp),
-          ),
-        ),
+            //title
+            title: Text(
+              widget.user.name!,
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16.sp),
+            ),
+            //subtitle
+            subtitle: Text(
+              widget.user.about!,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 14.sp),
+            ),
+            //leading icon
+            leading: CachedNetworkImage(
+              height: 20.h,
+              width: 20.w,
+              imageUrl: widget.user.image!,
+              // placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+            trailing: (!widget.user.isOnline!)
+                ? Container(
+                    height: 15.h,
+                    width: 15.w,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.r),
+                        color: Colors.greenAccent.shade400),
+                  )
+                : SizedBox()),
       ),
     );
   }
